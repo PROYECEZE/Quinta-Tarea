c6a26c6e61a9502548e8a4c8807158fbd35ba948
Ezequiel Blanco Mercado <ezeblamer@outlook.es>
2024-11-15 17:17:45 -0500

    feature/homeBlogs modified

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ tailwind.config = {
       colors: {
         darkBlue: '#506690',
         lightBlue: '#869AB8',
-        lighBlack: '#161C2D'
+        intenseBlue:'#335EEA',
+        lightBlack: '#161C2D',
+        strongBlue:'#12263F',
+        snowGray:'#6E84A3',
       },
       fontFamily: {
         roboto: ['roboto']
@@ -16,15 +19,31 @@ tailwind.config = {
         postSubtitle: ['21px', '32px'],
         postInfo:['17px', '26px'],
         '43px': ['42px', '52px'],
+        '52px': ['52px', '60px'],
       },
       container: {
         center: true,
         padding: '1rem',
       },
       maxWidth: {
-        'dimensions': '680px', 
+        'dimensions': '680px',
+        'size': '1040px', 
       },
+      boxShadow: {
+        '3xl': '0px 4px 4px 0px #00000040',
+      },
+      width: {
+        '100': '590px',
+      },
+      height: {
+        '90': '380px',
+      },
+      letterSpacing: {
+        oneLetter: '-0.1em',
+      }
     }
   }
 }
+
+
 
